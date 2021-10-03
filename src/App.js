8e4f0a3afb1e110c0e19b44e9fd16fee8debfb10
avinash409 <avinash.ponugupati@gmail.com>
2021-10-03 16:22:30 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          {/* <SearchBar /> */}
+        </nav>
+      </header>
+      {/* <PokemonList /> */}
+    </div>
+  );
+}
+
+export default App;
