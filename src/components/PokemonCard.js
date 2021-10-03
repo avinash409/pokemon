29c4418950bd9b6495adc8bbbce67bb83c36d801
avinash409 <avinash.ponugupati@gmail.com>
2021-10-03 18:09:40 +0100

    update Pokemon card compomnent to display details with styling

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -14,12 +14,34 @@ export const PokemonCard = ({ url, name }) => {
       <div>
         <figure>
           <img
-            className="pokemon-image"
+            className="pokemon-image-thumb"
             src={details?.sprites?.other?.["official-artwork"]?.front_default}
             alt={"pokemon-" + name}
           />
-          <figcaption>{name}</figcaption>
         </figure>
+        <div className="pokemon-details">
+          <div>
+            <label>Name: </label>
+            {name}
+          </div>
+          <div>
+            <label>Height:</label>
+            {details.height}
+          </div>
+          <div>
+            <label>Weight:</label>
+            {details.weight}
+          </div>
+          <div>
+            <label>Abilities:</label>
+            {details.abilities?.map((ab, i, arr) => (
+              <span key={ab.ability.name}>
+                {ab.ability.name}
+                {(i + 1) === arr.length ? "" : ", "}
+              </span>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
